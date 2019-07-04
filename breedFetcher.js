7c52ab1fcbf9ee71e2abb2126296cebc988853b3
susana araujo <26570969+sulamax@users.noreply.github.com>
2019-07-04 15:43:57 -0400

    Update breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,19 @@
 const request = require('request');
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-// const get = function(url, callback) {
-request.get(url, function(error, response, body) {
-  if (error) {
-    return console.log('Failed to request details: ', error);
-    // throw "Error getting URL "+error
-  }
-  const data = JSON.parse(body);
-
-
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${breedName}`);
-  }
-})
+const fetchBreedDescription = (breedName, callback) => {
+  request.get(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        const breed = JSON.parse(body)[0];
+        if (!breed) {
+          callback(null, "Breed not found!!!");
+        } else {
+          callback(null, breed.description);
+        }
+      }
+    }
+  );
+};
